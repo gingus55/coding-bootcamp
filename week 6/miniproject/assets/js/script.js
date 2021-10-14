93bd2fa8587f086373ee1de4300ed0f885aabb95
gingus55 <gingus55@hotmail.com>
2021-10-14 21:49:53 +0100

    finished project on api

--- week 6/miniproject/assets/js/script.js
+++ week 6/miniproject/assets/js/script.js
@@ -1,6 +1,7 @@
 console.log("howdy planet");
+const baseUrl = "https://api.github.com/users/gingus55/repos";
 
-fetch("https://api.github.com/users/gingus55/repos")
+fetch(baseUrl)
   .then(function (response) {
     // console.log();
     return response.json();
@@ -11,16 +12,19 @@ fetch("https://api.github.com/users/gingus55/repos")
 
     const container = $("#card-container");
     const createCardAndAppend = function (each) {
-      return `<div class="card" style="width: 18rem;">
-      <img src="..." class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="${each.name}">Card title</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
-    </div>`;
+      console.log("here i am");
+      const card = `<div class="card" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title}">${each.name}</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>`;
+
+      container.append(card);
     };
 
-    container.append(createCardAndAppend);
     data.forEach(createCardAndAppend);
   });
+
